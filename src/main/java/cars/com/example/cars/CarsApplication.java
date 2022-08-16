3ea838db1b12069f9dfbb6a787089454442052b8
Andre <triandrewicakono@gmail.com>
2022-08-16 15:06:41 +0700

    test online

--- src/main/java/cars/com/example/cars/CarsApplication.java
+++ src/main/java/cars/com/example/cars/CarsApplication.java
@@ -3,7 +3,7 @@ package cars.com.example.cars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,8 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CarsApplication {
 
 	public static void main(String[] args) {
-		// BCryptPasswordEncoder encode = new BCryptPasswordEncoder();
-		// System.out.println(encode.encode("admin"));
+		BCryptPasswordEncoder encode = new BCryptPasswordEncoder();
+		System.out.println(encode.encode("admin"));
 		SpringApplication.run(CarsApplication.class, args);
 	}
 
